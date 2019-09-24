0053614a644921a310b4740cc7d72ccd89a63f63
Kevin C. Escobedo <escobedo001@gmail.com>
2019-09-23 17:20:01 -0700

    Start of NPC class

---
+++ npc_class.py
@@ -0,0 +1,7 @@
+#Author: u/gonano4
+
+class NPC:
+    def __init__(self, name: str = "", race:str = "", life:float = 0.0):
+        self.name = name
+        self.race = race
+        self.life = life
