5fc42114495a07cb4a401793c65dbbc2552f92df
Kevin C. Escobedo <escobedo001@gmail.com>
2019-09-29 16:00:30 -0700

    Added item drop method

--- npc_class.py
+++ npc_class.py
@@ -3,6 +3,7 @@
 #TODO:
 #Should we make race into a class?
 #And make each different race have their own characteristics?
+from item_class import Item
 
 class NPC:
     def __init__(self, name: str = "", race:str = "", life:float = 0.0):
@@ -13,3 +14,6 @@ class NPC:
     def speak(self, dialogue:str):
         '''Returns dialogue for NPC to say'''
         return dialogue
+
+    def item_drop(self):
+        return Item("Iron Sword", 100, 50)
